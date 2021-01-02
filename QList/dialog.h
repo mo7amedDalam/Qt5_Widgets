942f4cf764a97be47c999180a14354fb7952a139
Mohamed dalam <mo7amed.dalam@gmail.com>
2021-01-02 15:55:18 +0200

    Save and load a list

--- QList/dialog.h
+++ QList/dialog.h
@@ -2,6 +2,10 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QMessageBox>
+#include <QSettings>
+#include <QVariant>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Dialog; }
@@ -15,7 +19,15 @@ public:
     Dialog(QWidget *parent = nullptr);
     ~Dialog();
 
+private slots:
+    void on_buttonBox_clicked(QAbstractButton *button);
+
+    void on_BtnAdd_clicked();
+
 private:
     Ui::Dialog *ui;
+    void init();
+    void load();
+    void save();
 };
 #endif // DIALOG_H
